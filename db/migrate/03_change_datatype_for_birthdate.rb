23a69f39a44f07231442c11491c9d1474d8e9a39
Ad Friedman <adam.friedman@flatironschool.com>
2019-04-11 14:54:02 -0400

    Done.

---
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -0,0 +1,5 @@
+class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
+  def change #column #(table_name, column_name, type)
+    change_column :students, :birthdate, :datetime
+  end
+end
